4be3f063cf9423d4ff372b6651c407680243e1bd
HGilman <hassenbike@gmail.com>
2025-12-05 02:36:14 +0500

    first day finished

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,40 @@
 fun main() {
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val commands = input.map {
+            it[0] to it.substring(1).toInt()
+        }
+        return calculateZeros(commands)
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val commands = input.map {
+            val direction = it[0]
+            val amount = it.substring(1).toInt()
+            (0 until amount).map { direction to 1}
+        }.flatten()
+        return calculateZeros(commands)
     }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    part1(testInput)
 
-    // Read the input from the `src/Day01.txt` file.
+//    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
+}
+
+fun calculateZeros(input: List<Pair<Char, Int>>): Int {
+    var zeroCounter = 0
+    var current = 50
+
+    for ((direction, amount) in input) {
+        current += if (direction == 'L') -amount else amount
+        current = current.mod(100)
+        if (current == 0) {
+            zeroCounter++
+        }
+    }
+    return zeroCounter
 }
